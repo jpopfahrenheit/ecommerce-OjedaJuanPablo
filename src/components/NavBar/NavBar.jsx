fbd60f7282672f6fb10c75f8272f4458970749d4
Juan Pablo Ojeda <jpop.fahrenheit@gmail.com>
2024-08-25 09:51:35 -0300

    Master

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,26 +1,26 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import { NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom';
 import { CartWidget } from "../CartWidget/CartWidget";
 import { Logo } from "../Logo/Logo";
 
 export const NavBar = () => (
-    <>
-        <Navbar bg="dark" data-bs-theme="dark">
-            <Container>
+    <Navbar bg="dark" data-bs-theme="dark">
+        <Container className="d-flex justify-content-between align-items-center">
+            <Navbar.Brand as={NavLink} to="/">
                 <Logo />
-                <Navbar.Brand as={NavLink} to="/">Zapatillas Center</Navbar.Brand>
-                <Nav className="me-auto">
-                    <Nav.Link as={NavLink} to="/categoria/Deportiva">Deportivas</Nav.Link>
-                    <Nav.Link as={NavLink} to="/categoria/Urbana">Urbanas</Nav.Link>
-                    <Nav.Link as={NavLink} to="/genero/Hombre">Hombres</Nav.Link>
-                    <Nav.Link as={NavLink} to="/genero/Mujer">Mujeres</Nav.Link>
-                    <Nav.Link as={NavLink} to="/genero/Chicos">Chicos</Nav.Link>
-                    <Nav.Link as={NavLink} to="/genero/Chicas">Chicas</Nav.Link>
-                </Nav>
-            </Container>
+                Zapatillas Center
+            </Navbar.Brand>
+            <Nav className="mx-auto">
+                <Nav.Link as={NavLink} to="/categoria/Deportiva">Deportivas</Nav.Link>
+                <Nav.Link as={NavLink} to="/categoria/Urbana">Urbanas</Nav.Link>
+                <Nav.Link as={NavLink} to="/genero/Hombre">Hombres</Nav.Link>
+                <Nav.Link as={NavLink} to="/genero/Mujer">Mujeres</Nav.Link>
+                <Nav.Link as={NavLink} to="/genero/Chicos">Chicos</Nav.Link>
+                <Nav.Link as={NavLink} to="/genero/Chicas">Chicas</Nav.Link>
+            </Nav>
             <CartWidget />
-        </Navbar>
-    </>
+        </Container>
+    </Navbar>
 );
